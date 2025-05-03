7ca8b3e723a0b66df1cc20d3d47cdc91e7f141b8
Seungju Seo <96508302+jujuinnnng@users.noreply.github.com>
2025-05-03 17:38:25 +0900

    [Bronze V] Title: A+B - 8, Time: 240 ms, Memory: 18912 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int count = sc.nextInt();
+
+        for (int i=0; i<count; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println("Case #"+(i+1)+": " + a + " + " + b + " = " +(a+b));
+        }
+        sc.close();
+    }
+}
